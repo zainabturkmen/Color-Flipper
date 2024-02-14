56a3df0929054a6d305631eb08ed75e10f64870f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-14 09:12:42 +0430

    randomNumber

--- hex.js
+++ hex.js
@@ -1,1 +1,14 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color')
+
+btn.addEventListener("click", function(){
+    const randomNum = getRndomNum();
+    document.body.style.backgroundColor = hex[randomNum]
+    color.textContent = hex[randomNum]
+});
+
+function getRndomNum(){
+   return Math.floor(Math.random()*hex.length)
+}

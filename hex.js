9a62f52d2ed15cc4549832e8699595a6f429cae7
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-14 09:47:10 +0430

    randomNumber

--- hex.js
+++ hex.js
@@ -8,7 +8,7 @@ btn.addEventListener("click", function(){
     let hexColor = "#";
     for(let i = 0; i < 6; i++){ 
         hexColor += hex[getRndomNum()];  
-    }
+    };
 
     color.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;

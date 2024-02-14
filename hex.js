c3eddef3fe285905f0c02870ca13a1d994c4dec9
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-14 09:46:59 +0430

    randomNumber

--- hex.js
+++ hex.js
@@ -1,7 +1,7 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.getElementById('btn');
-const color = document.querySelector('.color')
+const color = document.querySelector('.color');
 
 btn.addEventListener("click", function(){   
 

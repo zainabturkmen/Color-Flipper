bdb33663bd7b031a17e926bbb4f20c225b5f9db7
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-14 08:37:44 +0430

    randomNumber

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function(){
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber();
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
